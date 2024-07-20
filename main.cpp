830be6a48f3a01f6fd7e0a523fd0fa4d4a438191
franckessi <essitsondofranckpatrick@gmail.com>
2024-07-20 21:15:40 +0200

    final project

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "conex.h"
+#include <QApplication>
+#include <QMessageBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    conex c;
+        bool test=c.createconex();
+        if(test)
+        {w.show();
+            QMessageBox::information(nullptr, QObject::tr("database is open"),
+                        QObject::tr("connection successful.\n"
+                                    "Click Cancel to exit."), QMessageBox::Cancel);
+
+    }
+        else
+            QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                        QObject::tr("connection failed.\n"
+                                    "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+        return a.exec();
+    w.show();
+    return a.exec();
+}
